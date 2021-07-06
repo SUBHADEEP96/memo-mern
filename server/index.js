b53e5062097a87e77fd161be38abe37629a6d327
SUBHADEEP PAUL <48767231+SUBHADEEP96@users.noreply.github.com>
2021-07-07 01:07:44 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,7 +26,6 @@ app.use('/posts',postRoutes);
 app.use('/user',userRoutes);
 
 
-// const CONNECTION_URL = "mongodb+srv://subhadeep:paul2021@memories.kx5zp.mongodb.net/memories";
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
